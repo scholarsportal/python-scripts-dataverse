009d494106398eb330a27f5b67f1c612ada03237
lubitchv <victoria.lubitch@utoronto.ca>
2022-07-18 17:38:57 -0400

    subfolders

--- save_files_with_dirs/save_files_with_dirs.py
+++ save_files_with_dirs/save_files_with_dirs.py
@@ -1,11 +1,20 @@
 import requests
 import os
+import sys
 
-API_TOKEN = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-SERVER_URL = "https://demo.borealisdata.ca"
-PERSISTENT_ID = "doi:10.80240/FK2/UKKWRJ"
+API_TOKEN = ""
+SERVER_URL = "https://borealisdata.ca"
+PERSISTENT_ID = "doi:10.5683/SP3/PSWY62"
 
 def main():
+    number_of_args = len(sys.argv)
+    folders = set()
+    if number_of_args > 1:
+        folders.update(sys.argv[1:len(sys.argv)])
+        all=False
+    else:
+        all=True
+
     print(f"Start downloading files for {PERSISTENT_ID}")
     headers = {'X-Dataverse-key': API_TOKEN}
     url = SERVER_URL + "/api/datasets/:persistentId//versions/:latest/files?persistentId=" + PERSISTENT_ID
@@ -15,27 +24,38 @@ def main():
     if resp.status_code == 200:
         data = resp.json()['data']
         for dataset in data:
+            download = False
             id = dataset["dataFile"]["id"]
             filename = dataset["dataFile"]["filename"]
             if "directoryLabel" in dataset:
                 directoryLabel = dataset["directoryLabel"]
                 dr = "dataset/" + directoryLabel
+                if all:
+                    download = True
+                else:
+                    for element in folders:
+                        if (directoryLabel.startswith(element + "/") or directoryLabel == element):
+                            download = True
             else:
                 dr = "dataset"
-            name = dr + "/" + filename
-            if not os.path.isdir(dr):
-                os.makedirs(dr)
+                if not all and '.' not in folders:
+                    continue
+                download = True
+            if download:
+                name = dr + "/" + filename
+                if not os.path.isdir(dr):
+                    os.makedirs(dr)
 
-            url_file = SERVER_URL + "/api/access/datafile/" + str(id)
-            r = requests.get(url_file, headers=headers)
-            if r.status_code != 200:
-                print(f"Cannot download {filename} ")
-                continue
-            else:
-                print(name)
+                url_file = SERVER_URL + "/api/access/datafile/" + str(id)
+                r = requests.get(url_file, headers=headers)
+                if r.status_code != 200:
+                    print(f"Cannot download {filename} ")
+                    continue
+                else:
+                    print(name)
 
-            with open(name, 'wb') as f:
-                f.write(r.content)
+                with open(name, 'wb') as f:
+                    f.write(r.content)
     else:
         print("Cannot get the dataset")
 
