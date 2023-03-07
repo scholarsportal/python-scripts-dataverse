4d14250dba549f648689d054613b64000d4406d0
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2023-03-07 17:01:21 -0500

    logging

--- update_checksum/update_checksum.py
+++ update_checksum/update_checksum.py
@@ -4,10 +4,11 @@ from pyDataverse.api import NativeApi, DataAccessApi
 import psycopg2
 from psycopg2 import OperationalError
 import configparser
-import sys
+from datetime import datetime
 import os
 import time
 import requests
+import logging
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -71,11 +72,10 @@ def replace_file(result, identifier, replacement_file):
     split_strings = result[0][1].split(':')
     storage_id = split_strings[2]
     backet_name = split_strings[1][2::]
-    print(backet_name)
-    print(storage_id)
-    print(identifier)
+    logging.info("Backet name: " + backet_name)
+    logging.info("Storage_id: " + storage_id)
     file_name = "s3://" + backet_name + "/" + identifier + "/" + storage_id
-    print(file_name)
+    logging.info("Dataverse file name: " + file_name)
     cmd = "aws --endpoint-url " + cfg_dataverse['s3_endpoint'] + " s3 cp " + replacement_file + " " + file_name
     os.system(cmd)
 def find_storage_id(file_id):
@@ -119,8 +119,10 @@ def uningest_file(file_id, type):
     if type == "text/tab-separated-values":
         url = url_base + "/api/files/" + str(file_id) + "/uningest"
         resp = requests.post(url, headers=headers)
-        print(resp.status_code)
-
+        if resp.status_code != 200:
+            return False
+    else:
+        return True
 
 def reingest_file(originalFileFormat, file_ending, file_id):
     original = originalFileFormat.lower()
@@ -129,55 +131,80 @@ def reingest_file(originalFileFormat, file_ending, file_id):
             file_ending == 'tsv' or file_ending == 'xlxs' or file_ending == 'xls':
         url = url_base + "/api/files/" + str(file_id) + "/reingest"
         resp=requests.post(url, headers=headers)
-        print(resp.status_code)
-        print(resp.json())
-
+        if resp.status_code != 200:
+            logging.error("Error reingesting file {0}".format(file_id))
+            return False
+    return True
 def main():
+
+    now_date = datetime.now()
+    year = now_date.year
+    month = now_date.strftime("%b")
+    day = now_date.strftime("%d")
+
+    filename_log = "update_checksum_" + str(day) + "-" + month + "-" + str(year) + ".log"
+    logging.basicConfig(filename=filename_log, level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info("Program started")
     csv_file = open(cfg_dataverse['list_file'])
     reader = csv.reader(csv_file, delimiter=',', escapechar='\\', quotechar='"')
 
     for file in reader:
-        print(file[0])
+        logging.info(file[0])
         file_metadata = get_file_metadata(file[0])
-        print(file_metadata)
-        uningest_file(file[0], file_metadata[0][0])
+        if file_metadata != None and len(file_metadata) > 0 and file_metadata[0] != None and len(file_metadata[0][0])>0:
+            status_uningest=uningest_file(file[0], file_metadata[0][0])
+            if status_uningest:
 
-        #Get new file
-        resp = requests.get(file[1], allow_redirects=True)
-        if resp.status_code == 200:
-            open('temp_file', 'wb').write(resp.content)
+                #Get new file
+                resp = requests.get(file[1], allow_redirects=True)
+                if resp.status_code == 200:
+                    open('temp_file', 'wb').write(resp.content)
 
-            #Replace file part
-            result = find_storage_id(file[0])
-            owner_id = result[0][0]
-            time.sleep(1)
-            identifier = find_identifier(owner_id)
-            replace_file(result, identifier[0][0], "temp_file")
+                    #Replace file part
+                    result = find_storage_id(file[0])
+                    if result != None and len(result) > 0 and result[0] != None and len(result[0]) > 0:
+                        owner_id = result[0][0]
+                        identifier = find_identifier(owner_id)
+                        if identifier != None and len(identifier) > 0 and identifier[0] != None and len(identifier[0]) > 0:
+                            replace_file(result, identifier[0][0], "temp_file")
 
-            #Checksum
-            #type = magic.from_file('temp_file', mime=True)
-            #print(type)
-            readable_hash = hashlib.md5(resp.content).hexdigest()
-            print(readable_hash)
-            filesize = os.path.getsize('temp_file')
-            print(filesize)
-            #resp_meta = api.get_datafile_metadata(file[0])
-            #print(resp_meta.json())
-            time.sleep(1)
-            update_checksum(file[0], readable_hash, filesize, type)
+                            #Checksum
+                            #type = magic.from_file('temp_file', mime=True)
+                            #print(type)
+                            readable_hash = hashlib.md5(resp.content).hexdigest()
+                            logging.info("Hash : " + readable_hash)
+                            filesize = os.path.getsize('temp_file')
+                            logging.info("Filesize: " + filesize)
+                            #resp_meta = api.get_datafile_metadata(file[0])
+                            #print(resp_meta.json())
+                            update_checksum(file[0], readable_hash, filesize, type)
 
 
-            #Update file metadata (label)
-            filename_split = file[1].split('/')
-            filename = filename_split[len(filename_split)-1]
-            #update_file_metadata(file[0], filename)
+                            #Update file metadata (label)
+                            filename_split = file[1].split('/')
+                            filename = filename_split[len(filename_split)-1]
+                            #update_file_metadata(file[0], filename)
 
-            file_ending = filename.split(".")[1]
+                            file_ending_split = filename.split(".")
+                            if file_ending_split != None and  len(filename_split) > 1:
+                                file_ending = filename.split(".")[1]
+                            else:
+                                file_ending = None
 
-            reingest_file(file_metadata[0][0], file_ending, file[0])
+                            reingest_file(file_metadata[0][0], file_ending, file[0])
 
-            #Delete temp file
-            os.system("rm temp_file")
+                            #Delete temp file
+                            os.system("rm temp_file")
+                        else:
+                            logging.error("Cannot find PID for file {0} and dataset {1}".format(str(file[0]), str(owner_id)))
+                    else:
+                        logging.error("Cannot find storage_id for file {0}".format(str(file[0])))
+                else:
+                    logging.error("Cannot get file {0}".format(file[1]))
+            else:
+                logging.error("Cannot get file metadata for file {0}".format(str(file[0])))
 
     csv_file.close()
 
